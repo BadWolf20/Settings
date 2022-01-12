d0478e61ca32adbc019a3c913a143a3d43accabd
BadWolf20 <Roman.Pichugin20@gmail.com>
2022-01-12 17:13:55 +0700

    Реструкруризация кода, добавление уведомлений

--- Settings/MainViewController.swift
+++ Settings/MainViewController.swift
@@ -7,12 +7,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+
+class MainViewController: UIViewController {
 
     var set = [Setting]()
+    let notificationCenter = UNUserNotificationCenter.current()
+
+    // MARK: - Views
     lazy var tableView: UITableView = {
         let tableView = UITableView(frame: .zero, style: .grouped)
-        tableView.register(TableViewCell.self, forCellReuseIdentifier: "TableViewCell")
+        tableView.register(SettingsTableViewCell.self, forCellReuseIdentifier: "SettingsTableViewCell")
         tableView.frame = CGRect.init(origin: .zero, size: view.frame.size)
         tableView.rowHeight = 50
         tableView.dataSource = self
@@ -21,57 +25,44 @@ class ViewController: UIViewController {
         return tableView
     }()
 
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        //self.view.backgroundColor = UIColor.white
         self.navigationItem.title = "Settings"
-        //self.navigationController?.navigationBar.prefersLargeTitles = true
 
-
-        //self.tableView.register(TableViewCell.self, forCellReuseIdentifier: "TableViewCell")
-        //self.tableView.dataSource = self
-
-
+        askAccess()
         setupHierarchy()
-        setupLayout()
-        setupView()
-    }
-
-
-    // MARK: - Settings
-    private func setupHierarchy() {
-        view.addSubview(tableView)
-    }
-
-    private func setupLayout() {
-
-    }
-
-    private func setupView() {
-
-    }
-
-    private func updateLayout(with size: CGSize) {
-       tableView.frame = CGRect.init(origin: .zero, size: size)
     }
 
     override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
         super.viewWillTransition(to: size, with: coordinator)
         updateLayout(with: size)
     }
+
+    // MARK: - Settings
+    private func setupHierarchy() {
+        view.addSubview(tableView)
+    }
+
+    private func updateLayout(with size: CGSize) {
+       tableView.frame = CGRect.init(origin: .zero, size: size)
+    }
+
 }
 
+//  Добавление таблицы
+extension MainViewController: UITableViewDataSource, UITableViewDelegate {
 
-extension ViewController: UITableViewDataSource, UITableViewDelegate{
-
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath){
+    // При выборе строки
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
-
+        sendNotification(title: "You push '\(settingsList[getSelectedRowNumber()].name)' button")
         print("You push '\(settingsList[getSelectedRowNumber()].name)' button")
 
         moveToSetttingView(settingsList[getSelectedRowNumber()].name)
 
-        func getSelectedRowNumber() -> Int{
+        // Внутриние функции
+        func getSelectedRowNumber() -> Int {
             var selectedRowNumber = Int()
             switch indexPath.section {
             case 0:
@@ -84,11 +75,11 @@ extension ViewController: UITableViewDataSource, UITableViewDelegate{
             return selectedRowNumber
         }
 
-        func cot(_ sectionNum: Int) -> Int{
+        func cot(_ sectionNum: Int) -> Int {
             var c = Int()
             var out = Int()
-            for i in Block.allValues{
-                out += getSettingsSecttionLenght(block: i)
+            for i in Block.allValues {
+                out += Setting.getSettingsSectionLenght(block: i)
                 if c == sectionNum - 1 {
                     break
                 }
@@ -99,71 +90,78 @@ extension ViewController: UITableViewDataSource, UITableViewDelegate{
 
     }
 
+    // При момент до выбора строки (собираемся выбрать)
     func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
-        if indexPath.row == 0 && indexPath.section == 0{
+        if indexPath.row == 0 && indexPath.section == 0 {
             return nil
         }
         return indexPath
     }
 
-
-    func numberOfSections(in tableView: UITableView) -> Int{
+    // Количество секций
+    func numberOfSections(in tableView: UITableView) -> Int {
         return Setting.getSectionsCount()
     }
 
+    // Количество ячеек в каждой секции
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         switch section {
         case 0:
-            return getSettingsSecttionLenght(block: .fast)
+            return Setting.getSettingsSectionLenght(block: .fast)
         case 1:
-            return getSettingsSecttionLenght(block: .notifications)
+            return Setting.getSettingsSectionLenght(block: .notifications)
         case 2:
-            return getSettingsSecttionLenght(block: .main)
+            return Setting.getSettingsSectionLenght(block: .main)
         default:
             return 0
         }
     }
 
-
-
+    // Определение содержимого ячейки
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = self.tableView.dequeueReusableCell(withIdentifier: "TableViewCell", for: indexPath) as! TableViewCell
+        let cell = self.tableView.dequeueReusableCell(withIdentifier: "SettingsTableViewCell", for: indexPath) as! SettingsTableViewCell
 
-        switch indexPath.section{
+        switch indexPath.section {
         case 0:
-            set = getSettingsList(block: .fast)
+            set = Setting.getSettingsList(block: .fast)
         case 1:
-            set = getSettingsList(block: .notifications)
+            set = Setting.getSettingsList(block: .notifications)
         case 2:
-            set = getSettingsList(block: .main)
+            set = Setting.getSettingsList(block: .main)
         default:
-            set = getSettingsList(block: .fast)
+            set = Setting.getSettingsList(block: .fast)
         }
 
         let setChose = set[indexPath.row]
 
+        // Присвоение значений в ячейки
         var content = cell.defaultContentConfiguration()
-
         content.text = setChose.name
         content.image = setChose.imageMain
         content.imageProperties.maximumSize = CGSize(width: 30, height: 30)
-
         cell.contentConfiguration = content
 
-        let switchObj = UISwitch(frame: CGRect(x: 1, y: 1, width: 20, height: 20))
-        switchObj.isOn = false
-        switchObj.addTarget(self, action: #selector(tog(_:)), for: .valueChanged)
+        // Создание переключателя
+        lazy var switchObj: UISwitch = {
+            let switchObj = UISwitch(frame: CGRect(x: 1, y: 1, width: 20, height: 20))
+            switchObj.isOn = false
+            switchObj.addTarget(self, action: #selector(tog(_:)), for: .valueChanged)
 
-        if setChose.style == .check {
+            return switchObj
+        }()
+
+        // Определение стиля
+        switch setChose.style {
+        case .check:
             cell.accessoryView = switchObj
-        }
-        if setChose.style == .move {
+        case .move:
             cell.accessoryType = .disclosureIndicator
         }
 
         return cell
     }
 
+    // Переход на нужное View
     func moveToSetttingView(_ pageName: String) {
         switch pageName{
         case "Wi-Fi":
@@ -173,15 +171,17 @@ extension ViewController: UITableViewDataSource, UITableViewDelegate{
         }
     }
 
-    @objc private func tog(_: UISwitch){
-        print("swoth")
+    // Срабатывание при переключении переключателя
+    @objc private func tog(_ toggle: UISwitch) {
+        print("Airplane mode is \(toggle.isOn ? "on" : "off")")
     }
 }
 
-class TableViewCell: UITableViewCell {
+// Класс ячейки
+class SettingsTableViewCell: UITableViewCell {
+    // При повторном использовании
     override func prepareForReuse() {
         super.prepareForReuse()
         self.accessoryType = .none
     }
 }
-
